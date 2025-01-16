daa1fad9c1b4e727ecf37a3ee345105203fe6707
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2025-01-16 14:29:39 -0500

    fixed duplication due to JOIN in secm

--- SAFI_Courses.sql
+++ SAFI_Courses.sql
@@ -40,17 +40,17 @@ JOIN stu_acad_rec AS acad ON cal.prog = acad.prog AND cal.yr = acad.yr
 WHERE cal.prog = acad.prog AND cal.sess = 'FA' AND cal.yr = acad.yr;
 
 -- Main
-SELECT
+SELECT 
   cw.id AS ExternalStudentId,
   crs.prog AS ExternalProgramId,
   crs.crs_no AS ExternalCourseID,
   crs.title1 || crs.title2 || crs.title3 AS Description,
   CASE
     -- WHEN THEN 'Projected'
-    WHEN cw.stat='' THEN 'Scheduled'
-    WHEN cw.stat='' THEN 'Passed'
+    -- WHEN cw.stat='' THEN 'Scheduled'
+    -- WHEN cw.stat='' THEN 'Passed'
     WHEN cw.stat='W' THEN 'Withdrawn'
-    WHEN cw.stat='' THEN 'Failed'
+    -- WHEN cw.stat='' THEN 'Failed'
     WHEN cw.stat='D' THEN 'Incomplete'  -- D for dropped = incomplete?
     WHEN cw.stat='R' THEN 'Enrolled'  -- R for registered = enrolled?
   END AS SchedulingStatus,
@@ -72,16 +72,16 @@ SELECT
   END AS Modality,
   '' AS SapApplicable,  -- leave blank
   'FALSE' AS RepeatIndicator,  -- default False
-  be.term_begin_date AS AS TermStartDate,  -- same as start date?
+  be.term_begin_date AS TermStartDate,  -- same as start date?
   '' AS Schedule,  -- leave blank
-  be.term_begin_date AS AS ScheduledInstructionStartDate,  -- same as start date?
-  be.term_end_date AS AS ScheduledInstructionEndDate  -- same as end date?
+  be.term_begin_date AS ScheduledInstructionStartDate,  -- same as start date?
+  be.term_end_date AS ScheduledInstructionEndDate  -- same as end date?
  FROM cw_rec AS cw 
  JOIN crs_rec AS crs ON cw.crs_no=crs.crs_no AND cw.cat=crs.cat
  JOIN stu_acad_rec AS acad ON cw.id=acad.id AND cw.sess=acad.sess AND cw.yr=acad.yr AND cw.prog=acad.prog
- JOIN secmtg_rec AS secm ON cw.crs_no=sec.crs_no
- JOIN mtg_rec AS mtg ON sec.mtg_no=mtg.mtg_no
+ JOIN secmtg_rec AS secm ON cw.crs_no=secm.crs_no AND cw.cat=secm.cat AND cw.yr=secm.yr AND cw.sess=secm.sess AND cw.sec=secm.sec_no
+ JOIN mtg_rec AS mtg ON secm.mtg_no=mtg.mtg_no
  JOIN prog_enr_rec AS prog ON cw.id=prog.id
  JOIN acad_cal_rec AS cal ON acad.prog=cal.prog AND acad.sess=cal.sess AND acad.yr=cal.yr
  JOIN TempBeginEndDates AS be ON acad.id=be.id AND acad.sess=be.sess AND acad.yr=be.yr AND cal.subsess=be.subsess
- WHERE (prog.acst='RENR' OR prog.acst='ENRF' OR prog.acst='ENRP') AND acad.reg_hrs>0 AND acad.wd_code<>'WD' AND cw.id='443154'
+ WHERE (prog.acst='RENR' OR prog.acst='ENRF' OR prog.acst='ENRP') AND acad.reg_hrs>0 AND acad.wd_code<>'WD';
